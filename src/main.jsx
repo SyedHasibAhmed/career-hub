baa0736a533d30a5836e8e7a1f7c4ad6adb2b1b9
SyedHasibAhmed <syedhasibahmed11@gmail.com>
2024-03-11 00:13:38 +0600

    components update

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Root from './components/Root/Root';
-import Home from './components/Home/Home';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./components/Root/Root";
+import Home from "./components/Home/Home";
+import AppliedJobs from "./components/AppliedJobs/AppliedJobs";
+import ErrorPage from "./components/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
-    children : [
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
       {
-        path: '/',
-        element: <Home></Home>
-      }
-    ]
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/applied",
+        element: <AppliedJobs></AppliedJobs>,
+      },
+    ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
